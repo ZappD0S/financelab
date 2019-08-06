e950fd8fc21d2744904b8dc02959623b2db0f274
Zapp95 <gianlucazappavigna@gmail.com>
2019-08-06 17:44:50 +0200

    updated keras_lstm.pym, added attention.py

--- keras_lstm.py
+++ keras_lstm.py
@@ -3,9 +3,12 @@ import re
 import glob
 import numpy as np
 import keras
+from keras import backend as K
 from keras.models import Model
-from keras.layers import Input, Dense
+from keras.layers import Input, Dense, Permute, Lambda, RepeatVector, Flatten, multiply, concatenate
 from keras.layers.recurrent import LSTM
+from keras.layers import CuDNNLSTM
+from keras.layers.wrappers import Bidirectional
 # from utils import rolling_window
 import scipy.stats as spt
 from google.colab import drive
@@ -14,6 +17,19 @@ from google.colab import drive
 def rolling_window(x, window_size):
     stride = x.strides[0]
     return np.lib.stride_tricks.as_strided(x, (x.size - window_size + 1, window_size), (stride, stride))
+
+
+def attention_block(inputs, single_attention_vector=False):
+    input_dim, time_steps = inputs.shape[-2:]
+    a = Permute((2, 1))(inputs)
+    a = Dense(time_steps, activation='softmax')(a)
+    if single_attention_vector:
+        a = Lambda(lambda x: K.mean(x, axis=1), name='dim_reduction')(a)
+        a = RepeatVector(input_dim)(a)
+    a_probs = Permute((2, 1), name='attention_vec')(a)
+    output = multiply([inputs, a_probs], name='attention_mul')
+    # return output
+    return K.sum(output, axis=1)
 
 
 def compute_statistics(windowed_dlogp):
@@ -27,19 +43,29 @@ def compute_statistics(windowed_dlogp):
     statistics = (statistics - statistics.mean(axis=0)) / statistics.std(axis=0)
     return statistics
 
+# def create_model():
+#     x = Input(batch_shape=(None, lookbehind, 1))
+#     lstm1 =  LSTM(100, dropout=0.2, recurrent_dropout=0.15, return_sequences=True)(x)
+#     lstm2 = LSTM(100, dropout=0.2, recurrent_dropout=0.15)(lstm1)
+#     stats = Input(batch_shape=(None, 4))
+#     dense_input = keras.layers.concatenate([lstm2, stats], axis=1)
+#     output = Dense(3, activation='softmax')(dense_input)
+#     return Model(inputs=[x, stats], outputs=output)
+
 
 def create_model():
-    x = Input(batch_shape=(None, lookbehind, 1))
+    inputs = Input(batch_shape=(None, lookbehind, 1))
+    out = Bidirectional(LSTM(128, dropout=0.4, recurrent_dropout=0.4, activation='relu', return_sequences=True))(inputs)
+    out = Bidirectional(CuDNNLSTM(64, return_sequences=True))(out)
+    stats = Input(batcch_shape=(None, 4))
+    stats = RepeatVector(out.shape[1])(stats)
+    out = concatenate([out, stats], axis=-1)
+    out = attention_block(out)
 
-    lstm1 = LSTM(100, dropout=0.2, recurrent_dropout=0.15, return_sequences=True)(x)
-    lstm2 = LSTM(100, dropout=0.2, recurrent_dropout=0.15)(lstm1)
-
-    stats = Input(batch_shape=(None, 4))
-    dense_input = keras.layers.concatenate([lstm2, stats], axis=1)
-
-    output = Dense(3, activation='softmax')(dense_input)
-
-    return Model(inputs=[x, stats], outputs=output)
+    # dense_input = keras.layers.concatenate([lstm2, stats], axis=1)
+    out = Flatten()(out)
+    out = Dense(3, activation='softmax')(out)
+    return Model(inputs=[inputs], outputs=out)
 
 
 batch_size = 200
