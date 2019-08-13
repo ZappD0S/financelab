4bde68a0e0504771a41fb13fa67d3fb37350b9a4
Zapp95 <gianlucazappavigna@gmail.com>
2019-08-13 17:21:55 +0200

    updated train_data_gen.py (+1 squashed commits)
    Squashed commits:
    
    [1732de5] updated train_data_gen.py

--- train_data_gen.py
+++ train_data_gen.py
@@ -1,11 +1,11 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-from numba import jit, njit
-from utils import rolling_window
-from scipy import stats
+# import matplotlib.pyplot as plt
+from numba import njit, prange
+# from utils import rolling_window
+# from scipy import stats
 from scipy.signal import correlate
-import h5py
+# import h5py
 from fracdiff import get_weights
 
 
@@ -16,22 +16,11 @@ def autocorr(x):
     return result[result.size // 2:] / x.size
 
 
-# def build_mu_sigma(mu_sigma):
-
-#     for i in range(lookbehind - min_steps):
-#         # TODO: bisogna invertire la direzione!
-#         mu_sigma[:, i, 0] = np.mean(windowed_logrets[:, :min_steps + i], axis=1)
-#         mu_sigma[:, i, 1] = np.std(windowed_logrets[:, :min_steps + i], axis=1)
-#         mu_sigma[:, i, 2] = stats.skew(windowed_logrets[:, :min_steps + i], axis=1)
-#         mu_sigma[:, i, 3] = stats.kurtosis(windowed_logrets[:, :min_steps + i], axis=1)
-
-
-@njit
+@njit(parallel=True)
 def build_output_arr(y):
-    for i in range(len(buy_close) - lookahead):
+    for i in prange(len(buy_close) - lookahead):
         sell_threshold_reached = False
         buy_threshold_reached = False
-
         profitable = False
 
         for j in range(lookahead):
@@ -64,47 +53,19 @@ timeframe = '1min'
 
 df = pd.read_pickle('tick_data/eurusd_2018.pkl')
 df = df.resample(timeframe).ohlc().dropna()
-
-# minutely = df.resample('1min').last().dropna()['buy'].apply(np.log).values
-# hourly = df.resample('1h').last().dropna()['buy'].apply(np.log).values
-# daily = df.resample('1d').last().dropna()['buy'].apply(np.log).values
-
-
-# logrets = np.diff(np.log(df['buy', 'close'].values))
-
-# logrets = np.diff(np.log(df['buy'].values))
-
-# windowed_logrets = rolling_window(logrets, lookbehind)
-
-# fname = f'train_data/mu_sigma_w{lookbehind}.dat'
-# fname = f'train_data/X_w{lookbehind}.dat'
-# fname = f'train_data/X_w{lookbehind}.hdf5'
-
-# np.save(fname)
-# mu_sigma = np.memmap(fname, dtype='float64', mode='r+', shape=(*windowed_logrets.shape, 2))
-# X = np.memmap(fname, dtype='float64', mode='r+',
-#                      shape=(windowed_logrets.shape[0], windowed_logrets.shape[1] - min_steps, 4))
-
-
-# X = np.memmap(fname, dtype='float32', mode='w+',
-#                      shape=(windowed_logrets.shape[0], windowed_logrets.shape[1], 5))
+df.to_pickle('tick_data/eurusd_2018_1min.pkl')
+raise Exception
 
 buy_close, buy_high = df['buy', 'close'].values, df['buy', 'high'].values
 sell_close, sell_low = df['sell', 'close'].values, df['sell', 'low'].values
 
-y = np.empty(len(buy_close) - lookahead, dtype='int8')
+y = np.empty(len(buy_close) - lookahead, dtype='int16')
 # build_output_arr(y)
+# np.save('y.npy', y)
 
-# y = y[:-lookahead-1]
 y = y[lookbehind - 1:]
 
 weights = get_weights(0.6, lookbehind).flatten()
 dlogp = np.convolve(np.log(df['buy', 'close'].values), weights, mode='valid')[:-lookahead]
 
 np.savez(f'train_data/train_data_tf{timeframe}.npz', dlogp=dlogp, y=y)
-
-# build_mu_sigma(mu_sigma, windowed_logrets)
-# d = dict(zip(df.columns.values, df.to_numpy().T))
-# y = np.empty(len(df.index) - lookahead, dtype='int8')
-# build_output_arr(y, df)
-# build_output_arr(y)
