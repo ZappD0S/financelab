8eba06e65aa87201bbd73d88c7d839be77d2c864
Zapp95 <gianlucazappavigna@gmail.com>
2019-09-11 17:01:51 +0200

    updated train_data_gen

--- train_data_gen.py
+++ train_data_gen.py
@@ -68,13 +68,16 @@ def compute_good(log_gmin):
 
         buy_counts.append(buy_count)
         sell_counts.append(sell_count)
-        if np.sum(sell_mask | buy_mask) / len(logp) >= 0.99:
+        # if np.sum(~(sell_mask | buy_mask)) / buy_mask.size < 1e-5:
+        if np.all(sell_mask | buy_mask) or shift > 300:
         # if (sum(buy_counts) + sum(sell_counts)) / len(logp) >= 0.99:
         # if (cum_buy_count + cum_sell_count) / len(logp) >= 0.99:
             print(shift)
             break
     # return buy_mask, sell_mask
     # return buy_counts, sell_counts
+    buy_counts = np.asarray(buy_counts)
+    sell_counts = np.asarray(sell_counts)
     return (buy_mask, sell_mask), (buy_counts, sell_counts)
 
 
@@ -84,7 +87,8 @@ min_steps = 10
 lookbehind = 6000
 lookahead = 100
 loss_threshold = 0.9998
-timeframe = '1s'
+# timeframe = '1s'
+timeframe = '30s'
 
 
 df = pd.read_pickle('tick_data/eurusd_2018.pkl')
@@ -99,6 +103,7 @@ logp = np.log(df['buy', 'close'].values)
 # logp = np.log(df['buy'].values)
 # buy_mask, sell_mask = compute_good(1.7e-5)
 (buy_mask, sell_mask), (buy_counts, sell_counts) = compute_good(1.2e-4)
+# raise Exception
 to_skip = len(buy_counts)
 # buy_mask, sell_mask = compute_good(6.1e-5)
 
@@ -106,15 +111,17 @@ y = np.empty(len(logp) - 1, dtype='uint8')
 y[buy_mask & sell_mask] = 0
 y[buy_mask & ~sell_mask] = 1
 y[~buy_mask & sell_mask] = 2
-y[~(buy_mask | sell_mask)] = 3
+if np.any(~(buy_mask | sell_mask)):
+    y[~(buy_mask | sell_mask)] = 3
 # build_output_arr(y)
-y = y[:-to_skip]
+y = y[:-to_skip + 1]
 logp = logp[:-to_skip]
+
+assert y.size == logp.size
 # y = y[lookbehind - 1:]
 
 # weights = get_weights(d, lookbehind).flatten()
 # dlogp = np.convolve(np.log(df['buy', 'close'].values), weights, mode='valid')[:-lookahead]
-
 # assert dlogp.size == y.size
 
 with h5py.File(f'train_data/train_data_tf{timeframe}_lfz.h5', 'w') as f:
