633198fc6bea8ab53e104ebb2e6de197f6a0a814
zapp <gianlucazappavigna@gmail.com>
2020-07-04 17:11:05 +0200

    Completed the cuda kernel and fixed remaining bugs so that now it runs. But it's still too slow, we need to fix it.

--- cuda_tests.py
+++ cuda_tests.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from numba import njit, prange, cuda, float64
+from numba import njit, prange, cuda, float64, float32
 
 
 @njit(parallel=True)
@@ -20,18 +20,13 @@ def compute_stuff(p, lower, upper):
     return res
 
 
-tpb = (32, 4 * 4)
-
-multiplier = 1000
-
-
 @cuda.jit
 def compute_stuff_cuda(p, lower, upper, out):
-    shared_out = cuda.shared.array((4, 4, 2), dtype=float64)
+    shared_out = cuda.shared.array((l_tpb, u_tpb, 2), dtype=float32)
 
-    shared_p = cuda.shared.array(tpb * multiplier, dype=float64)
-    shared_lower = cuda.shared.array(4, dype=float64)
-    shared_upper = cuda.shared.array(4, dype=float64)
+    shared_p = cuda.shared.array(frame_size, dtype=float32)
+    shared_lower = cuda.shared.array(l_tpb, dtype=float32)
+    shared_upper = cuda.shared.array(u_tpb, dtype=float32)
     i0, lu = cuda.grid(2)
 
     # (lower.size, upper.size)
@@ -42,33 +37,30 @@ def compute_stuff_cuda(p, lower, upper, out):
     if l >= lower.size or u >= upper.size:
         return
 
-    max_i0 = p.size - p.size % shared_p.size
+    frames_per_grid = p.size // frame_size
 
-    if i0 >= max_i0:
+    if i0 >= frames_per_grid * frame_size:
         return
 
     ti0 = cuda.threadIdx.x
     tlu = cuda.threadIdx.y
 
-    tl = tlu // 4
-    tu = tlu % 4
+    tl = tlu // u_tpb
+    tu = tlu % u_tpb
 
     shared_lower[tl] = lower[tl]
     shared_upper[tu] = upper[tu]
-    shared_out[tl, tu] = 0
 
-    left_frames = (max_i0 - i0) // shared_p.size + 1
+    left_frames = (frames_per_grid * frame_size - i0 + frame_size - 1) // frame_size
 
     found = False
     for frame in range(left_frames):
         for i in range(multiplier):
-            shared_p[frame * shared_p.size + ti0 * multiplier + i] = p[
-                i0 + frame * shared_p.size + ti0 * (multiplier - 1) + i
-            ]
+            shared_p[ti0 * multiplier + i] = p[i0 + frame * frame_size + ti0 * (multiplier - 1) + i]
 
         cuda.syncthreads()
 
-        for ti in range(ti0, shared_p.size):
+        for ti in range(ti0, frame_size):
             if shared_p[ti] - shared_p[ti0] < shared_lower[tl]:
                 shared_out[tl, tu, 0] += 1
                 found = True
@@ -81,8 +73,6 @@ def compute_stuff_cuda(p, lower, upper, out):
         if cuda.syncthreads_and(found):
             break
 
-    # questo probabilmente non serve
-    cuda.syncthreads()
     cuda.atomic.add(out, (l, u, 0), shared_out[tl, tu, 0])
     cuda.atomic.add(out, (l, u, 1), shared_out[tl, tu, 1])
     # questo funziona?
@@ -90,10 +80,34 @@ def compute_stuff_cuda(p, lower, upper, out):
 
 
 df = pd.read_parquet("tick_data/eurusd_2019.parquet.gzip")
-p = df["buy"].values
+# df = pd.read_parquet("drive/My Drive/train_data/eurusd_2019.parquet.gzip")
 
-lower = -np.linspace(1e-5, 1e-1, 200)
-upper = np.linspace(1e-5, 1e-1, 200)
+p = cuda.to_device(df["buy"].values.astype("float32"))
 
-thresholds = np.stack((lower, upper), axis=-1)
-# res = compute_stuff(p, thresholds)
+lower = cuda.to_device(-np.linspace(1e-5, 1e-1, 200, dtype="float32"))
+upper = cuda.to_device(np.linspace(1e-5, 1e-1, 200, dtype="float32"))
+
+
+i_tpb = 32
+l_tpb = 4
+u_tpb = 4
+
+multiplier = 150
+
+frame_size = i_tpb * multiplier
+
+frames_per_grid = p.size // frame_size
+i_bpg = (frames_per_grid * frame_size + i_tpb - 1) // i_tpb
+
+l_bpg = (lower.size + l_tpb - 1) // l_tpb
+u_bpg = (upper.size + u_tpb - 1) // u_tpb
+
+tpb = (i_tpb, l_tpb * u_tpb)
+bpg = (i_bpg, l_bpg * u_bpg)
+
+d_out = cuda.to_device(np.zeros((l_tpb * l_bpg, u_tpb * u_bpg, 2), dtype="float32"))
+
+compute_stuff_cuda[bpg, tpb](p, lower, upper, d_out)
+cuda.synchronize()
+
+out = d_out.copy_to_host()
